91a107df8b49d2729668d0b8ca9f41fb77e34fb3
Anderson <anderson.calderoncampos@gmail.com>
2025-09-19 15:55:19 -0500

    Actualizar imagenes 01

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images:{
 
-             formats:['image/avif','image/webp'],
+             formats:['image/avif','image/webp','image/png'],
              domains:['jacekjeznach.com',"https://formsubmit.co/"]
 
          }
